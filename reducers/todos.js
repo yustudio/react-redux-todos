bc25ef3a846c433418b50aee7a79ef3f1a5ec7a5
desongyu <desong.e.yu@gmail.com>
2017-01-27 13:16:31 -0500

    Normalizing the State Shape with byId and allId

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,55 +1,86 @@
-const todo = (state = {}, action) => {
+import { combineReducers } from 'redux';
+import todo from './todo';
+
+// return copy of the map between id and todo items, with updated todo for action.id
+const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
     case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state
-      }
-
-      return Object.assign({}, state, {
-        completed: !state.completed
-      })
-
-    default:
-      return state
-  }
-}
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
       console.log("todos reducer's state: " + JSON.stringify(state))
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      )
+      return {  // now return new lookup table object rather than array
+        ...state,  // need to install babel-plugin-transform-object-rest-spread and in babelrc transform-object-rest-spread
+        [action.id]: todo(state[action.id], action)
+      };
+    
     default:
       return state
   }
 }
+
+// const todos = (state = [], action) => {
+//   switch (action.type) {
+//     case 'ADD_TODO':
+//       console.log("todos reducer's state: " + JSON.stringify(state))
+//       return [
+//         ...state,
+//         todo(undefined, action)
+//       ]
+//     case 'TOGGLE_TODO':
+//       return state.map(t =>
+//         todo(t, action)
+//       )
+//     default:
+//       return state
+//   }
+// }
+
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.id];
+    default:
+      return state;
+  }
+}
+
+const todos = combineReducers({
+  byId,
+  allIds
+});
 
 export default todos;
 
-// Put here since reducer knows about state todo
-// This is a Selector funct since it selects from current state
+// internally used selector
+const getAllTodos = (state) => {
+  // return all todos by id
+  return state.allIds.map(id => state.byId[id]);
+}
+
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
   switch (filter) {
     case 'all':
-      return state
+      return allTodos
     case 'completed':
-      return state.filter(t => t.completed)
+      return allTodos.filter(t => t.completed)
     case 'active':
-      return state.filter(t => !t.completed)
+      return allTodos.filter(t => !t.completed)
     default: 
       throw new Error('Unknown filter: ${filter}')
   }
 }
+
+// Put here since reducer knows about state todo
+// This is a Selector funct since it selects from current state
+// export const getVisibleTodos = (state, filter) => {
+//   switch (filter) {
+//     case 'all':
+//       return state
+//     case 'completed':
+//       return state.filter(t => t.completed)
+//     case 'active':
+//       return state.filter(t => !t.completed)
+//     default: 
+//       throw new Error('Unknown filter: ${filter}')
+//   }
+// }
