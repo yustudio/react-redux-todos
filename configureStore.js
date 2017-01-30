52644463633dc7733f00864f12f0c62d8c40625d
desongyu <desong.e.yu@gmail.com>
2017-01-30 16:45:00 -0500

    apply redux middleware

--- configureStore.js
+++ configureStore.js
@@ -1,66 +1,95 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers/index';
 //import { loadState, saveState, deleteState } from './localStorage';
 //import throttle from 'lodash/throttle';
 
-const addLoggingToDispatch = (store) => {
-	const rawDispatch = store.dispatch;
+// const logger = (store) => { // could use (store) => (next)=> (action) => {}
+// 	return (next) => {  // currying: let user input what property to pass, e.g. dispatch
+// 	//const next = store.dispatch;  // next points to original dispatch
 
-	if (!console.group) {
-		return rawDispatch;
-	}
+// 		if (!console.group) {
+// 			return next;
+// 		}
 
-	// if given action, return new dispatch function with logging
-	return (action) => {
-		console.group(action.type);
+// 		// if given action, return new dispatch function with logging
+// 		return (action) => {
+// 			console.group(action.type);
 
-		console.log('%c prev state', 'color: gray', store.getState());
-		console.log('%c action', 'color: blue', action);
-		const returnValue = rawDispatch(action);
-		console.log('%c next state', 'color: green', store.getState());
+// 			console.log('%c prev state', 'color: gray', store.getState());
+// 			console.log('%c action', 'color: blue', action);
+// 			const returnValue = next(action);
+// 			console.log('%c next state', 'color: green', store.getState());
 
-		console.groupEnd(action.type);
-		return returnValue;  // return new dispatch function with logging
-	}
-}
+// 			console.groupEnd(action.type);
+// 			return returnValue;  // return new dispatch function with logging
+// 		}
+// 	}
+// }
 
-const addPromiseSupportToDispatch = (store) => {
-	const rawDispatch = store.dispatch;
-	return (action) => {
-		if (typeof action.then === 'function') {  // action is promise
-			return action.then(rawDispatch);
-		}
-		return rawDispatch(action);
-	}
-}
+// const promise = (store) => {   
+// 	//console.log("addPromiseSupportToDispatch")
+// 	return (next) => { 
+// 		//const next = store.dispatch;  // next dispatch 
+// 		return (action) => {
+// 			if (typeof action.then === 'function') {  // action is promise
+// 				return action.then(next);
+// 			}
+// 			return next(action);
+// 		}
+// 	}
+// }
+
+// const wrapDispatchWithmiddlewaress = (store, middlewares) => {
+// 	// reverse here since want to recgonize action promise last
+// 	middlewares.slice().reverse().forEach(middleware =>   
+// 		store.dispatch = middleware(store)(store.dispatch)  // store.dispatch is composed
+// 	)	
+// }
 
 const configureStore = () => {
 
 	//deleteState();  // only used to remove the saved state
 	//const persistedState = loadState();  // comment out so to use fetch data
 
-	const store = createStore(todoApp);
-	//const store = createStore(todoApp, persistedState);
-	console.log(store.getState());
+	const middlewares = [promise]; // specifiy based on order action propagate through middleware
 
 	if (process.env.NODE_ENV !== 'production') {
-		store.dispatch = addLoggingToDispatch(store);
+		middlewares.push(createLogger());
+		//middlewares.push(logger);
+		//store.dispatch = addLoggingToDispatch(store);
 	}	
 
-	// // when store changes, saveState gets called
-	// store.subscribe(throttle(() => {
-	// 	saveState({
-	// 		// persist only the todos state not UI filter state, which
-	// 		// gets reset to showall by the reducer.
-	// 		todos: store.getState().todos
-	// 		});
-	// 		// since everytime saveState calls stringify is expensive, 
-	// 		// we throttle to save once every second  
-	// }, 1000)); 
+	return createStore (
+		todoApp,
+		applyMiddleware	(...middlewares) // return an enhancer
+		);
+	// const store = createStore(
+	// 	todoApp,
+	// 	// persistedState goes here if needed
+	// 	applyMiddleware(...middlewares));
+		//const store = createStore(todoApp);
+		//const store = createStore(todoApp, persistedState);
+	console.log(store.getState());
+	
 
-	store.dispatch = addPromiseSupportToDispatch(store);
+		// // when store changes, saveState gets called
+		// store.subscribe(throttle(() => {
+		// 	saveState({
+		// 		// persist only the todos state not UI filter state, which
+		// 		// gets reset to showall by the reducer.
+		// 		todos: store.getState().todos
+		// 		});
+		// 		// since everytime saveState calls stringify is expensive, 
+		// 		// we throttle to save once every second  
+		// }, 1000)); 
 
-	return store;
+		//middlewares.push(promise);
+		//store.dispatch = addPromiseSupportToDispatch(store);
+		//wrapDispatchWithmiddlewaress(store, middlewares);
+
+	//return store;
 }
 
 // can create many instances of store in tests
